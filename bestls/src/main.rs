2005641d7d3d27496de31ef1b41250a72b8d76ba
Naman Balaji <namanb487@gmail.com>
2025-12-22 23:39:02 +0100

    added custom ls

---
+++ bestls/src/main.rs
@@ -0,0 +1,108 @@
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use owo_colors::OwoColorize;
+use serde::Serialize;
+use std::{
+    fs::{self},
+    path::{Path, PathBuf},
+};
+use strum_macros::Display;
+use tabled::{
+    Table, Tabled,
+    settings::{
+        Color, Style,
+        object::{Columns, Rows},
+    },
+};
+
+#[derive(Debug, Display, Serialize)]
+enum EntryType {
+    File,
+    Dir,
+}
+
+#[derive(Debug, Tabled, Serialize)]
+struct FileEntry {
+    #[tabled{rename="Name"}]
+    name: String,
+    #[tabled{rename="Type"}]
+    e_type: EntryType,
+    #[tabled{rename="Size B"}]
+    len_bytes: u64,
+    modified: String,
+}
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = "Best ls command ever")]
+struct Cli {
+    path: Option<PathBuf>,
+    #[arg(short, long)]
+    json: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let path = cli.path.unwrap_or(PathBuf::from("."));
+
+    if let Ok(does_exist) = fs::exists(&path) {
+        if does_exist {
+            let get_files = get_files(&path);
+            if cli.json {
+                println!(
+                    "{}",
+                    serde_json::to_string(&get_files).unwrap_or("cannot parse json".to_string())
+                )
+            } else {
+                print_table(&get_files);
+            }
+        } else {
+            println!("{}", "Path does not exists".red());
+        }
+    } else {
+        println!("{}", "error reading directory".red());
+    }
+}
+
+fn get_files(path: &Path) -> Vec<FileEntry> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for file in read_dir.flatten() {
+            map_data(file, &mut data);
+        }
+    }
+    data
+}
+
+fn map_data(file: fs::DirEntry, data: &mut Vec<FileEntry>) {
+    if let Ok(meta) = fs::metadata(file.path()) {
+        data.push(FileEntry {
+            name: file
+                .file_name()
+                .into_string()
+                .unwrap_or("unknown name".into()),
+            e_type: if meta.is_dir() {
+                EntryType::Dir
+            } else {
+                EntryType::File
+            },
+            len_bytes: meta.len(),
+            modified: if let Ok(modi) = meta.modified() {
+                let date: DateTime<Utc> = modi.into();
+                format!("{}", date.format("%a %b %e %Y"))
+            } else {
+                String::default()
+            },
+        });
+    }
+}
+
+fn print_table(files: &Vec<FileEntry>) {
+    let mut table = Table::new(files);
+    table.with(Style::rounded());
+    table.modify(Columns::first(), Color::FG_BRIGHT_CYAN);
+    table.modify(Columns::one(2), Color::FG_BRIGHT_MAGENTA);
+    table.modify(Columns::one(3), Color::FG_BRIGHT_YELLOW);
+    table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+    println!("{}", table);
+}
